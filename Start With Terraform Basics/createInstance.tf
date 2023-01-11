2f6dce23d68d538c62afa4491bac942c5a22e686
Hamza BADRE <hbadre@example.com>
2023-01-11 21:24:42 +0100

    modify keys

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,7 +1,7 @@
 
 provider "aws" {
-  access_key="AKIAUDAOT56DKXK374NS" 
-  secret_key = "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"
+  access_key="AKIAUDAOT56DN5U4FIGO" 
+  secret_key = "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"
   region = "eu-central-1"
 }
 
