053b1b0755d6791ef819b5af14a14ea79e1c1f66
Hamza BADRE <hbadre@example.com>
2023-01-11 23:58:31 +0100

    separate 2"

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,9 +1,4 @@
 
-provider "aws" {
-  access_key="AKIAUDAOT56DN5U4FIGO" 
-  secret_key = "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"
-  region = "eu-central-1"
-}
 
 resource "aws_instance" "MyHB-Instance1"{
   ami="ami-0039da1f3917fa8e3"
