f9114728c819c21f937ec9d562123684ec8a5e5b
Hamza BADRE <hbadre@example.com>
2023-01-12 19:11:41 +0100

    map variable

--- Terraform Variables Detailed Explanation/variables.tf
+++ Terraform Variables Detailed Explanation/variables.tf
@@ -7,5 +7,16 @@ variable "Security_Groups" {
   type= list
 #eu-central-1        CLB-SG              launch-wizard-1	   instance-sg-2	
   default= ["sg-0682e6febb66951e0","sg-06bbfd5eb88ae28a1","sg-0af18c497c390f7ef"]
+}
 
+variable "AMIS" {
+  type=map
+  default = {
+     eu-central-1 = "ami-0039da1f3917fa8e3"
+     eu-west-1    = "ami-0f29c8402f8cce65c"
+     eu-west-2    = "ami-09a2a0f7d2db8baca"
+     eu-west-3    = "ami-052f10f1c45aa2155"
+
+
+  }
 }
