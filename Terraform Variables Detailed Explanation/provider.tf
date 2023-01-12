af3cde38d49f27513f17377b6b189e7c7c42aa82
Hamza BADRE <hbadre@example.com>
2023-01-12 14:11:25 +0100

    resolving errors in vars provider.tf 2

--- Terraform Variables Detailed Explanation/provider.tf
+++ Terraform Variables Detailed Explanation/provider.tf
@@ -1,6 +1,6 @@
 
 provider "aws" {
-  access_key = "${var.AWS.ACCESS_KEY}"
-  secret_key = "${var.AWS.SECRET_KEY}"
-  region     = "${var.AWS.REGION}"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
+  region     = "${var.AWS_REGION}"
 }
